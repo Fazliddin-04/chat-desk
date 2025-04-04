2f6b4d25257431859c57f688eac3ee7e7b877a5b
Fazlyddeen <fazlddn4@gmail.com>
2025-04-04 23:57:30 +0500

    fixed: import configs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,15 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import alias from '@rollup/plugin-alias'
+import path from 'path'
 
 const config = {
-  input: './src/main.js',
+  input: './index.ts',
   output: {
-    file: './build/bundle.min.js',
-    format: 'iife',
+    file: './dist/index.js',
+    format: 'esm',
     sourcemap: true,
   },
-  plugins: [resolve(), commonjs(), terser()],
+  plugins: [
+    alias({
+      entries: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    }),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
 }
 
 export default config
